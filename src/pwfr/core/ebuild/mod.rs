ae0557b0fa08533090a924bd7fbd3d13bee6b843
Boris Vinogradov <no111u3@gmail.com>
2019-10-03 22:34:47 +0300

    Adding core::Ebuild proto - create empty tests

--- src/pwfr/core/ebuild/mod.rs
+++ src/pwfr/core/ebuild/mod.rs
@@ -3,4 +3,24 @@
 //! Main functionality of for ebuild entity
 
 // Ebuild entity
+#[derive(Debug, PartialEq)]
 pub struct Ebuild {}
+
+impl Ebuild {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create() {
+        let ebuild_one = Ebuild {};
+        let ebuild_two = Ebuild::new();
+
+        assert_eq!(ebuild_one, ebuild_two);
+    }
+}
