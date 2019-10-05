063c098884a471ece5aacb02a1befbce1e302ed6
Boris Vinogradov <no111u3@gmail.com>
2019-10-05 14:31:47 +0300

    Adding Utility/FdFile - raw file descriptor holder

--- src/pwfr/core/ebuild/utils.rs
+++ src/pwfr/core/ebuild/utils.rs
@@ -3,5 +3,76 @@
 //! Service types and utility functions
 //! Inspired by https://crates.io/crates/nsh (github: https://github.com/seiyanuta/nsh)
 
+use std::io::Write;
+use std::os::unix::io::RawFd;
+
+use nix::unistd;
+
+/// `File`-like object but doesn't close the `fd`.
+#[derive(Debug, PartialEq)]
+pub struct FdFile {
+    fd: RawFd,
+}
+
+impl FdFile {
+    pub fn new(fd: RawFd) -> FdFile {
+        FdFile { fd }
+    }
+
+    pub fn read_line(&self) -> Option<String> {
+        let mut line = Vec::new();
+        loop {
+            let mut ch = vec![0; 1];
+            match unistd::read(self.fd, &mut ch) {
+                // EOF
+                Ok(read_len) if read_len == 0 => break,
+                // Read a character.
+                Ok(_) => {
+                    if ch[0] == 0x0a
+                    /* newline */
+                    {
+                        break;
+                    }
+
+                    line.push(ch[0]);
+                }
+                // Something went wrong.
+                Err(err) => {
+                    trace!("read_line: error: {:?}", err);
+                    break;
+                }
+            }
+        }
+
+        if line.is_empty() {
+            None
+        } else {
+            Some(String::from_utf8(line).expect("binary data is not yet supported"))
+        }
+    }
+}
+
+impl Write for FdFile {
+    #[inline]
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let len = unistd::write(self.fd, buf).expect("failed to write");
+        Ok(len)
+    }
+
+    #[inline]
+    fn flush(&mut self) -> std::io::Result<()> {
+        unistd::fsync(self.fd).ok();
+        Ok(())
+    }
+}
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create() {
+        let fd_file = FdFile::new(0);
+        assert_eq!(fd_file, FdFile { fd: 0 });
+    }
+}
--- src/pwfr/lib.rs
+++ src/pwfr/lib.rs
@@ -3,6 +3,8 @@
 //! Core library of project - contains entities, core functionality and main facilities
 
 #[macro_use]
+extern crate log;
+#[macro_use]
 extern crate pest_derive;
 
 pub mod core;
